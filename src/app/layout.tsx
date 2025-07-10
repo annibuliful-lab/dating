2dc4c5058e14720656fd4ae3c8fc294a8fe55793
annibuliful <lagmanmc@gmail.com>
2025-07-10 17:33:32 +0700

    fix: missing font subsets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,9 @@ import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
 const inter = Inter({
   weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
 });
+
 export default function RootLayout({
   children,
 }: {

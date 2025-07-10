0c0e64334f7deef6877bc7c264a0a75bf807bc98
annibuliful <lagmanmc@gmail.com>
2025-07-10 17:31:14 +0700

    feat: chore feed page

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,22 +4,25 @@ import { v7 } from 'uuid';
 
 export async function POST(request: Request) {
   const { username, password } = await request.json();
-  console.log({ username, password });
 
-  const { data, error } = await supabase.from('User').insert({
-    id: v7(),
-    email: username,
-    username,
-    passwordHash: password,
-    fullName: v7(),
-    updatedAt: new Date().toUTCString(),
-  });
+  const userId = v7();
+  const { data: user, error } = await supabase
+    .from('User')
+    .insert({
+      id: userId,
+      email: username,
+      username,
+      passwordHash: password,
+      fullName: v7(),
+      updatedAt: new Date().toUTCString(),
+    })
+    .select('*');
 
   if (error) {
     return NextResponse.json(
       {
         message: 'error',
-        error,
+        error: [error],
       },
       {
         status: 500,
@@ -27,5 +30,34 @@ export async function POST(request: Request) {
     );
   }
 
-  return NextResponse.json({ message: 'success', data });
+  const {
+    data: insertedOAuthAccount,
+    error: insertedOAuthAccountError,
+  } = await supabase
+    .from('OAuthAccount')
+    .insert({
+      id: v7() as string,
+      type: 'oauth',
+      provider: 'credentials',
+      providerAccountId: userId,
+      userId: userId,
+    })
+    .select('*');
+
+  if (insertedOAuthAccountError) {
+    return NextResponse.json(
+      {
+        message: 'error',
+        error: [error, insertedOAuthAccountError],
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return NextResponse.json({
+    message: 'success',
+    data: { user, insertedOAuthAccount },
+  });
 }
---
+++ src/app/feed/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button, Container, Text } from '@mantine/core';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+function FeedPage() {
+  const router = useRouter();
+  const { status, data } = useSession();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push('/');
+    }
+  }, [router, status]);
+
+  return (
+    <Container>
+      <Text ta="center">In-progress</Text>
+
+      <Text>
+        Information
+        <br />
+        {data && JSON.stringify(data)}
+      </Text>
+      <Button
+        onClick={async () => {
+          await signOut();
+          await router.push('/');
+        }}
+        display="block"
+        mx="auto"
+      >
+        Sign out
+      </Button>
+    </Container>
+  );
+}
+
+export default FeedPage;

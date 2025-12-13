7eed935ad7e107853cdac55589067c096846b719
annibuliful <lagmanmc@gmail.com>
2025-12-13 10:51:17 +0700

    fix: build

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -152,7 +152,7 @@ export default function LoginPage() {
         </Text>
 
         <Text size="sm" ta="center" c="dimmed">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link
             href="/signup"
             style={{ color: 'white', fontWeight: 500 }}
